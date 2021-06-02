74985da5c52a3aa02ed63491da0f2c5798d4135d
shilinj <shijlin@126.com>
2021-06-02 16:00:16 +0800

    update select sort code

--- algorithm/selectSort.go
+++ algorithm/selectSort.go
@@ -2,6 +2,11 @@ package algorithm
 
 import "fmt"
 
+/*
+	选择排序
+选择第一个元素作为最小数，与其他元素进行比较，如果其他元素小于该数，那么最小值的就是其他元素（注意索引的变化）
+*/
+
 func selectionSort(arr []int) {
 	length := len(arr)
 	for i := 0; i < length; i++ {
@@ -11,8 +16,8 @@ func selectionSort(arr []int) {
 				min = j
 			}
 		}
-		arr[i], arr[min] = arr[min], arr[i]
-		// fmt.Printf("第%v次选择：%v\n", i+1, arr)
+		arr[i], arr[min] = arr[min], arr[i] // 将最小值和初始参考值交换位置
+		fmt.Printf("第%v次选择：%v\n", i+1, arr)
 	}
 	fmt.Println(arr)
 }
